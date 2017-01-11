c50e1fa19833d7b08ef4c4a6f4782a1aa8c8ac44
mrosanes <mrosanes@cells.es>
2017-01-11 15:04:45 +0100

    Remove jumps
    New class created to begin with a post alignment procedure.
    The new class will be used to remove jumps after during a second
    alignment. This will be done with an interpolation of move vectors.

---
+++ alignlib/alignspectrum/post_align.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+"""
+(C) Copyright 2017 Marc Rosanes
+The program is distributed under the terms of the
+GNU General Public License.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import cv2
+import numpy as np
+from alignlib.align import Alignment
+from alignlib.utils import Utils
+
+
+class PostAlignRemoveJumps():
+
+    def __init__(self, alignfile, normalizedfile):
+        pass
+
+
