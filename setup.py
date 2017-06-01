cfabb841165483b9680cc03a4e443b85e3618b81
mrosanes <mrosanes@cells.es>
2017-06-01 16:09:06 +0200

    Add setup.py for using setuptools

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+# ctalign setup.py to be able to use setuptools
+
+# The version is updated automatically with bumpversion
+# Do not update manually
+__version = '2.2.0-alpha' 
+
+
+setup(
+    name='ctalign',
+    version=__version,
+    packages=find_packages(),
+    entry_points={
+	'console_scripts': [
+        'ctalign = ctalignlib.ctalign:main']
+        }
+)
+
+
