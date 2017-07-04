da0d7248ecdb00df37d46ec19c42a259b4ff30af
mrosanes <mrosanes@cells.es>
2017-07-04 12:50:54 +0200

    Bump version 2.3.0-alpha to 2.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 # The version is updated automatically with bumpversion
 # Do not update manually
-__version = '2.3.0-alpha' 
+__version = '2.3.0' 
 
 
 long_description = """ ctalign project has born with the initial idea of make an 
